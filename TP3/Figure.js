04224d42fc057aad596a95a76735408d89ee74ad
Olivia <anaoliviatodesco@gmail.com>
2024-10-16 19:29:06 -0300

    classes y anotaciones

--- TP3/Figure.js
+++ TP3/Figure.js
@@ -3,6 +3,7 @@ class Figure{
     constructor(x, y){
         this.canvas = document.getElementById('canvas');
         this.ctx = this.canvas.getContext("2d");
+        this.resaltado = false; //para indicar que aun no se resalto en el tablero, aun no se ha dibuajdo
         this.x = x;
         this.y = y;
     }
@@ -11,7 +12,10 @@ class Figure{
     }
 
     getPosition(){
-        return {x: getX(), y: getY()};
+        return {
+            x: getX(), 
+            y: getY()
+        };
     }
 
     getX(){
@@ -25,5 +29,12 @@ class Figure{
     setFill(){
         this.fill = fill;
     }
+
+    //agregar una imagen de fondo
+    /*
+    let image = ctx.createPattern(img,"repeat");
+    ctx.rect(0, 0, 150, 100);
+    ctx.fillStyle = image;
+    ctx.fill();*/
 
 }
---
+++ TP3/main.js
@@ -0,0 +1,7 @@
+
+//onMouseUp
+//onMouseDown()..
+//addCircle
+//addBoard()
+
+//al dibujar figura tambbien hay que borrarla! sino se hacen 29i38294824 figuras
