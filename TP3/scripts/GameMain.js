3ed74b697670587c53601a6b8466c2b7c39f06cd
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2024-10-29 20:35:22 -0300

    elimino comentarios

--- TP3/scripts/GameMain.js
+++ TP3/scripts/GameMain.js
@@ -8,6 +8,4 @@ canvas_width = 16 / 9 * canvas_height;
 canvas.width = canvas_width;
 canvas.height = canvas_height;
 
-let audio = new Audio('static/audio/Ambiente.mp3');
-audio.play();
 let game = new Game();
