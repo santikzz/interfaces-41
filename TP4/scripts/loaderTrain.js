ec7fbcd68091fe489f10474b2bf4598abcee1567
Olivia Todesco <104026140+SrtaLiv@users.noreply.github.com>
2024-11-09 00:34:02 -0300

    las nubes solo andan a la resolucion 1250, el tren falta acomodar al centro pero funciona

---
+++ TP4/scripts/loaderTrain.js
@@ -0,0 +1,32 @@
+const train = document.querySelector(".train-img");
+const clouds = document.querySelectorAll('.cloud');
+const loaderTrain = document.querySelector("loader-train");
+
+function startTrain() {
+    train.classList.add("move")
+    setTimeout(() => {
+        animateClouds();
+    }, 3000);
+  
+}
+
+function animateClouds() {
+    clouds.forEach(cloud => {
+        let cloudPosition = cloud.getBoundingClientRect();
+
+        // Si la nube sale de la pantalla por la izquierda
+        if (cloudPosition.right < 0) {
+            // Mueve la nube al lado derecho
+            cloud.style.left = `${window.innerWidth + 100}px`;  // Un poco más allá para empezar a moverse inmediatamente
+        }
+        else {
+            cloud.style.left = `${cloudPosition.left - 1}px`;
+        }
+    });
+
+    requestAnimationFrame(animateClouds);
+}
+
+startTrain();
+
+
